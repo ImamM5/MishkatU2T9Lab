7f89b8180285b1d0872ea2c0a66c7cebe31d98fe
Imam Mishkat <imammishkat@gmail.com>
2022-10-17 18:58:09 -0400

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Circle newCircle = new Circle(5.0);
+        System.out.println(newCircle.getInfo());
+        newCircle.setRadius(9.2);
+        System.out.println(newCircle.getInfo());
+
+        //second test case
+        newCircle.setRadius(22.1);
+        System.out.println(newCircle.getInfo());
+
     }
 }
